19012ab4a11be0228ff95e70898f4310874415f2
HuyNg <elvizhuy@gmail.com>
2023-09-10 00:57:10 +0700

    .

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app.use('/public/movies', express.static('public/movies'));
 // app.use("/public/movies",express.static(publicPathDirectory))
 app.use("/api/v1", rootRouter)
 // app.use(Fingerprint())
+
+///////////// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,*');
+    // Set to true if you need the website to in
+
 app.listen(3456, async () => {
     console.log('App is running on http://localhost:3456')
     try {
